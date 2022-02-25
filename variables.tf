d91ed5bfea00125f3fd1418a1fd451318f1df59d
geethasathyamurthy <43596586+geethasathyamurthy@users.noreply.github.com>
2022-02-25 21:13:43 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,91 @@
+variable "bucket_name" {
+  default = "a-standard-bucket-at-ams-firewall"
+}
+
+variable "resource_group_name" {
+  default = "default"
+}
+
+variable "storage" {
+  default = "standard"
+}
+
+variable "region" {
+  default = "us"
+}
+
+variable "single_site_loc" {
+  default = "sjc04"
+}
+
+variable "archive_ruleid" {
+  default = ""
+}
+
+variable "regional_loc" {
+  default = "us-south"
+}
+
+variable "archive_days" {
+  default = 0
+}
+
+variable "archive_types" {
+  default = "ACCELERATED"
+}
+
+variable "expire_ruleid" {
+  default = ""
+}
+
+variable "expire_days" {
+  default = 1
+}
+
+variable "expire_date" {
+  default = ""
+}
+
+variable "expire_prefix" {
+  default = ""
+}
+
+variable "nc_exp_ruleid" {
+  default = "test-obj-ver-exp-3"
+}
+
+variable "nc_exp_days" {
+  default = 1
+}
+
+variable "nc_exp_prefix" {
+  default = ""
+}
+
+variable "abort_mpu_ruleid" {
+  default = "test-abort_mpu-5"
+}
+
+variable "abort_mpu_days_init" {
+  default = 1
+}
+
+variable "abort_mpu_prefix" {
+  default = ""
+}
+
+variable "default_retention" {
+  default = "0"
+}
+
+variable "minimum_retention" {
+  default = "0"
+}
+
+variable "maximum_retention" {
+  default = "1"
+}
+
+variable "quota" {
+  default = "1"
+}
